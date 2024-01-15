bc5dd0e4d285e5df32f56a1b566eb3b092407e73
Bett <bettpatrick080@gmail.com>
2024-01-15 23:54:25 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit",(event) => {
+    event.preventDefault()
+    const inputValue = document.querySelector('input#searchByID').value
+    console.log(inputValue)
+
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+        .then((res) => res.json())
+        .then((movies) => {
+            console.log(movies)
+            const title = document.querySelector("h4")
+            const summary = document.querySelector("p")
+
+            title.innerHTML = movies.title
+            summary.innerHTML = movies.summary
+
+        })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
